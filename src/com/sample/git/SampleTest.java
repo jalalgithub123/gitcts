ba7b73e382a91892c1ad482faa7fcc915360146c
jalal8068 <jalalahamad9@gmail.com>
2019-04-10 12:07:06 +0530

    sandhya commit

--- src/com/sample/git/SampleTest.java
+++ src/com/sample/git/SampleTest.java
@@ -2,6 +2,6 @@ package com.sample.git;
 
 public class SampleTest {
 public static void main(String[] args) {
-	System.out.println("Welcome to java");
+	System.out.println("Welcome to java field");
 }
 }
